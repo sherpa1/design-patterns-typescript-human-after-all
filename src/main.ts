72f020fdb8c8acd94e79b06f554c0b912d875fb9
Alexandre Leroux <alex@sherpa.one>
2022-10-16 00:02:30 +0200

    Human can't communicate with Alien

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 class Human {
-    private _firstname: string;
-    private _lastname: string;
+    protected _firstname: string;
+    protected _lastname: string;
 
-    constructor(pFirstname: string, pLastname: string) {
-        this._firstname = pFirstname;
-        this._lastname = pLastname;
+    constructor(firstname: string, lastname: string) {
+        this._firstname = firstname;
+        this._lastname = lastname;
     }
 
     fullname(): string {
@@ -37,6 +37,32 @@ class Human {
     }
 }
 
+class Alien {
+    protected _name: string;
+
+    constructor(name: string) {
+        this._name = name;
+    }
+
+    s4y(something: string): string {
+        return `- ${this._name} 54y5 : "${something}"`;
+    }
+
+    public get name(): string {
+        return this._name;
+    }
+    public set name(value: string) {
+        this._name = value;
+    }
+
+}
+
+class AlienAdapterToHuman {
+    constructor(alien: Alien) {
+
+    }
+}
+
 function main() {
     const john_doe = new Human("John", "Doe");
     console.log(john_doe.say('Hello, World !'));
@@ -45,6 +71,15 @@ function main() {
     console.log(john_doe.ask('What is your name ?', james_white));
 
     console.log(james_white.answer(`My name is ${james_white.fullname()}`, john_doe));
+
+    const hal_9000 = new Alien('HAL9000');
+    console.log(hal_9000.s4y(`@&$*%+=£`));
+
+    try {
+        john_doe.ask('Where are you coming from ?', hal_9000);
+    } catch (error) {
+        console.error(error);
+    }
 
 }
 
