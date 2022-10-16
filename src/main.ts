c155bbb0e157b7444fc54c7e5912260d869b50e8
Alexandre Leroux <alex@sherpa.one>
2022-10-16 11:12:14 +0200

    add Dialog

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 interface ICommunicant {
-    _firstname: string;
-    _lastname: string;
     fullname(): string;
     say(something: string): string;
-    answer(something: string, someone: ICommunicant): string;
-    ask(something: string, someone: ICommunicant): string;
 }
 
-class Human {
-    protected _firstname: string;
-    protected _lastname: string;
+class Dialog {
+    static log(who: ICommunicant, what: string, toWho?: ICommunicant) {
+        let str: string = `- ${who.fullname()} says `;
+
+        if (toWho) str += `to ${toWho.fullname()} `;
+        str += `: "${what}"`;
+
+        console.log(str);
+    }
+}
+
+class Human implements ICommunicant {
+    _firstname: string;
+    _lastname: string;
 
     constructor(firstname: string, lastname: string) {
         this._firstname = firstname;
@@ -21,15 +28,7 @@ class Human {
     }
 
     say(something: string): string {
-        return `- ${this.fullname()} says : "${something}"`;
-    }
-
-    answer(something: string, someone: Human): string {
-        return `- ${this.fullname()} answers to ${someone.fullname()} : "${something}"`;
-    }
-
-    ask(something: string, someone: Human): string {
-        return `- ${this.fullname()} asks to ${someone.fullname()} : "${something}"`;
+        return something;
     }
 
     public get firstname(): string {
@@ -54,7 +53,7 @@ class Alien {
     }
 
     s4y(something: string): string {
-        return `- ${this._name} 54y5 : "${something}"`;
+        return something;
     }
 
     public get name(): string {
@@ -66,26 +65,43 @@ class Alien {
 
 }
 
-class AlienAdapterToHuman {
-    constructor(alien: Alien) {
+class AlienAdapterToHuman implements ICommunicant {
 
+    private _alien: Alien;
+
+    constructor(alien: Alien) {
+        this._alien = alien;
     }
+
+    fullname(): string {
+        return `${this._alien.name}`;
+    }
+
+    say(something: string): string {
+        return `${this._alien.s4y(something)}`;
+    }
+
 }
 
 function main() {
     const john_doe = new Human("John", "Doe");
-    console.log(john_doe.say('Hello, World !'));
 
-    const james_white = new Human("James", "White");
-    console.log(john_doe.ask('What is your name ?', james_white));
+    Dialog.log(john_doe, john_doe.say('Hello, World !'));
 
-    console.log(james_white.answer(`My name is ${james_white.fullname()}`, john_doe));
+    const dave_bowman = new Human("Dave", "Bowman");
+
+    Dialog.log(john_doe, john_doe.say('What is your name ?'), dave_bowman);
+
+    Dialog.log(dave_bowman, dave_bowman.say(`My name is ${dave_bowman.fullname()}`), john_doe);
 
     const hal_9000 = new Alien('HAL9000');
-    console.log(hal_9000.s4y(`@&$*%+=£`));
+
+    const adapterToHuman = new AlienAdapterToHuman(hal_9000);
+    Dialog.log(adapterToHuman, adapterToHuman.say(`@&$*%+=£`))
 
     try {
-        john_doe.ask('Where are you coming from ?', hal_9000);
+        Dialog.log(john_doe, john_doe.say('Where are you coming from ?'), adapterToHuman);
+        Dialog.log(adapterToHuman, adapterToHuman.say("I come from Mars"), john_doe);
     } catch (error) {
         console.error(error);
     }
